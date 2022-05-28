8673608f919f68918f37b515a3ada3095600f10e
José Silva <josegdasilvajunior@gmail.com>
2022-05-28 15:35:30 -0300

    Atualização do banco de dados

--- sql/sql.sql
+++ sql/sql.sql
@@ -24,3 +24,22 @@ create table valores(
     foreign key(fk_id) references usuarios(id),
     foreign key(fk_depara) references depara_padrao(depara)
 );
+
+
+
+insert into usuarios(usuario, senha) values('admin', 'admin');
+insert into usuarios(usuario, senha) values('usuario', 'usuario');
+insert into depara_padra(tipo, depara) values('Compra', 201);
+insert into depara_padra(tipo, depara) values('Gasto', 301);
+insert into depara_padra(tipo, depara) values('Investimento', 401);
+insert into depara_padra(tipo, depara) values('Despeza', 501);
+insert into depara_padra(tipo, depara) values('Drogas', 601);
+insert into valores(preco, data_compra, fk_id, fk_depara) values(2012.02, '2021-01-02', 1, 201);
+insert into valores(preco, data_compra, fk_id, fk_depara) values(2012.02, '2022-01-02', 1, 301);
+insert into valores(preco, data_compra, fk_id, fk_depara) values(6042.02, '2020-01-02', 1, 301);
+insert into valores(preco, data_compra, fk_id, fk_depara) values(1000.02, '2019-01-02', 1, 201);
+insert into valores(preco, data_compra, fk_id, fk_depara) values(500.02, '2018-01-02', 1, 401);
+
+
+
+
